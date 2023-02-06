2eabc809b1e84bea9057d710b5a6ac3d5fb983a0
Muhmammad Anas <78796163+MuhammadAnasAli@users.noreply.github.com>
2023-02-06 06:49:59 +0500

    Update team.tsx
    team h update

--- components/team/team.tsx
+++ components/team/team.tsx
@@ -4,7 +4,7 @@ import ResponsiveGrid from './teambox';
 function Team() {
     return (
         <div className='team' id='team'>
-            <h2>Our Team</h2>
+            <h2>Our Executives</h2>
             <div className='team_boxes'>
                 <ResponsiveGrid />
             </div>
